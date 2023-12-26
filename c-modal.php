a8d481a8b5e946ba86d58f2b8c359d17e4e9fc3c
anuja2016 <anujat@coreflexsolutions.com>
2023-12-26 11:36:34 +0530

    Modal

---
+++ c-modal.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'header.php';
+?>
+
+
+<div class="cModal">
+    <div class="cModalBody">
+        <div class="cModalHeader"></div>
+        <div class="cModalFooter"></div>
+    </div>    
+<div>
+
+
+
+<script></script>
+
+
+<?php
+include_once 'footer.php';
+?>
+
+
