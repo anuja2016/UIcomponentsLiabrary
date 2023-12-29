5d86f53c25ad83554482feff62b3bdd4af9f87c1
nikita <nikitag@coreflexsolutions.com>
2023-12-29 11:48:23 +0530

    Dropdown and modal UI code updated

---
+++ c-dropdown.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'header.php';
+?>
+
+<h2 class="cText-center">Dropdown Button</h2>
+<div class="cDropdown ">
+  <button onclick="myFunction()" class="cDropbtn cText-center">Dropdown</button>
+  <div id="cDropdown" class="cDropdown-content">
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+  </div>
+</div>
+
+
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("cDropdown").classList.toggle("cShow");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.cDropbtn')) {
+    var dropdowns = document.getElementsByClassName("cDropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('cShow')) {
+        openDropdown.classList.remove('cShow');
+      }
+    }
+  }
+}
+</script>
+<?php
+include_once 'footer.php';
+?>
--- c-modal.php
+++ c-modal.php
@@ -3,18 +3,71 @@ include_once 'header.php';
 ?>
 
 
-<div class="cModal">
+<!-- <div class="cModal">
     <div class="cModalBody">
         <div class="cModalHeader"></div>
         <div class="cModalFooter"></div>
     </div>    
-<div>
+<div> -->
 
 
+<!-- Modal top-->
+<h2 class="cText-center">Modal with Header and Footer</h2>
 
-<script></script>
+<!-- Trigger/Open The Modal -->
+<div class="cText-center">
+<button id="myBtn" class="cBtn-open">Open Modal</button>
+</div>
+<!-- The Modal -->
+<div id="myModal" class="cModal">
+
+  <!-- Modal content -->
+  <div class="cModal-content">
+    <div class="cModal-header">
+      <span class="cClose">&times;</span>
+      <h2>Modal Header</h2>
+    </div>
+    <div class="cModal-body">
+      <p>Some text in the Modal Body</p>
+      <p>Some other text...</p>
+    </div>
+    <div class="cModal-footer">
+      <h3>Modal Footer</h3>
+    </div>
+  </div>
+
+</div>
 
 
+<script>
+    // Get top modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("cClose")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == cModal) {
+    modal.style.display = "none";
+  }
+}
+
+
+</script>
 <?php
 include_once 'footer.php';
 ?>
